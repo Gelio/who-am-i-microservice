4f0209ed75c63b457fe21103199a21f80d476fe2
Gelio <voreny.gelio@gmail.com>
2016-01-28 19:41:06 +0100

    added data fetching and returning the correct response

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ var express = require('express'),
     app = express();
 
 app.get('/*', function(req, res) {
-    res.end('Hello world');
+    var responseObject = {
+        ipaddress: req.ip,
+        language: 'unknown',
+        software: /\([^()]*\)/.exec(req.headers['user-agent'])[0]
+    };
+
+    // Get the firsr language from the list
+    if (req.headers['accept-language']) {
+        var languages = req.headers['accept-language'].split(';');
+        if (languages.length > 0)
+            responseObject.language = languages;
+    }
+
+    // Remove the parentheses from software
+    responseObject.software = responseObject.software.slice(1, responseObject.software.length-1);
+
+    res.json(responseObject);
 });
 
 app.listen(port, function() {
